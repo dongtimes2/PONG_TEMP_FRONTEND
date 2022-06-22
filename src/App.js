fb6a0d232bb689b50baaa52d6779797d15af7efc
dongtimes2 <dongtimes2@gmail.com>
2022-06-23 08:56:59 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
+import { io } from 'socket.io-client';
 
 function App() {
+  const socket = useMemo(() => {
+    return io(process.env.REACT_APP_SERVER_URL);
+  }, [])
+
+  const [x, setValue] = useState(0);
+  const [alpha, setAlpha] = useState(0);
+  const [beta, setBeta] = useState(0);
+  const [gamma, setGamma] = useState(0);
+
+  const handleMotion = (event) => {
+    const x = event.acceleration.x;
+    // console.log(event)
+    setValue(x);
+  };
+
+  const handleOrientation = (event) => {
+    const alphaValue = event.alpha;
+    const betaValue = event.beta;
+    const gammaValue = event.gamma;
+
+    setAlpha(alphaValue);
+    setBeta(betaValue);
+    setGamma(gammaValue);
+  };
+
+  useEffect(() => {
+    window.addEventListener('devicemotion', handleMotion, false);
+    window.addEventListener('deviceorientation', handleOrientation, false);
+  }, [x, alpha, beta, gamma]);
+
+  // function handleMotion(event) {
+  //   console.log(event);
+  // }
+
+  // window.addEventListener('devicemotion', handleMotion);
+
+  const handleButtonClick = () => {
+    const response = new Date();
+    socket.emit('button', response);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello world</h1>
+      <p id='temp'></p>
+      <p>{x}</p>
+      <p>알파_팽이: {alpha}</p>
+      <p>베타_넘어지기: {beta}</p>
+      <p>감마_뒤집기: {gamma}</p>
+      <button onClick={handleButtonClick}>버튼</button>
+    </>
   );
 }
 
